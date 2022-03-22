8f54ae9c4f0e708ec399e5e6227431f7b78ee6ec
Mahesh <mahesh.nandavaram96@gmail.com>
2022-03-22 11:30:38 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+
+// const App = {
+//     data(){
+//         return{
+//             name:"Awesome"
+//         }
+//     }
+// }
+
+// Vue.createApp(App).mount('#intro')
+
+Vue.createApp({
+    data(){
+        return{
+            name:"Awesome"
+        }
+    }
+}).mount("#intro")
+
